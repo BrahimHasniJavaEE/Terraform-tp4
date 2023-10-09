aa56f67133a98f14f089fb9200fe27b800bb4c2d
Brahim HASNI <brahim.hasni.bellili@gmail.com>
2023-10-09 22:54:40 +0100

    first commit

---
+++ ReadMe.txt
@@ -0,0 +1,4 @@
+Le Tp numero 4:Deployer nginx et enregistrer l'IP
+Formation Terraform 
+centre de Formation : EazyTraining
+Formateur : Ulrich Steve NOUMSI 
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "instancetype" {
+    type = string
+    description = "set aws instance type"
+    default = "t2.nano"
+}
+variable "aws_common_tag" {
+    type =map(any)
+    description = "tag"
+    default = {
+        name="ec2-Brahim-HASNI"
+    } 
+}
+
+
+
+
+
